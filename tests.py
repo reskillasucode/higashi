e62ed4600f1323c170e5eae3251fe6899fc924ec
root <root@Mega>
2023-04-26 21:15:13 +0900

    second

--- tests.py
+++ tests.py
@@ -1,7 +1,9 @@
 from django.test import SimpleTestCase
 
+
 class TemplateTest(SimpleTestCase):
     def test_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
         self.assertContains(response, 'Welcome to my website!')
+
