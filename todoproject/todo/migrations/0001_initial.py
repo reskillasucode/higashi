af3c389a90f45a339aafc58cce5fe89cf6c60447
root <root@Mega>
2023-05-09 17:02:29 +0900

    base.htmlまで完成しました

--- todoproject/todo/migrations/0001_initial.py
+++ todoproject/todo/migrations/0001_initial.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='TodoModel',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=100)),
                 ('memo', models.TextField()),
             ],
