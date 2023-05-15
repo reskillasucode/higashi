64b6a6aa992edde2e68741a2a8b7bc42bbb706a9
root <root@Mega>
2023-05-15 18:11:59 +0900

    APIサーバーの学習1が終わりました

--- Bookproject/book/views.py
+++ Bookproject/book/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-from rest_framework.views import APIView
+from rest_framework.generics import ListCreateAPIView
 from .models import Book
+from .serializers import BookSerializer
 
 # Create your views here.
 
@@ -9,5 +10,7 @@ class BookList(ListView):
     template_name = 'list.html'
     model = Book
 
-class BookListAPI(APIView):
+class BookListAPI(ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
     permission_classes = []
